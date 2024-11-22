f18c0c8c4fb91692ae0dcc69ae82975c38fc23e4
prantika <prantikamutsuddi47@gmail.com>
2024-11-23 00:47:48 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,423 @@
+
+
+$(function(){
+  var mixer = mixitup('.mix', {
+    "animation": {
+      "duration": 629,
+      "nudge": false,
+      "reverseOut": false,
+      "effects": "fade translateZ(-57px)"
+  }
+  
+
+
+});
+  $('.parent_pro').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.parent_pro'
+  });
+  $('.child_pro').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.child_pro',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+  
+})
+
+$(function(){
+  let dec = document.querySelector(".dec");
+let result = document.querySelector(".result");
+let inc = document.querySelector(".inc");
+
+inc.addEventListener("click", function(){
+    
+    if(result.value < 5){
+        result.value = Number(result.value) + 1;
+        
+    }else{
+        // inc.style.cursor = `not-allowed`
+         alert("You have reached maximum limit")
+    }
+})
+
+dec.addEventListener("click",()=>{
+
+   if(result.value>1){
+    result.value = Number(result.value) - 1
+   }else{
+    dec.style.cursor = `not-allowed`
+   }
+
+})
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  centerPadding: '0px',
+  arrows: true,
+  prevArrow: '<span class="left"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow:'<span class="right"><i class="fa-solid fa-chevron-right"></i></span>',
+  focusOnSelect: true
+});
+
+
+})
+
+
+
+
+
+$(function(){
+  // shop details slick
+  $('.parent_arrival').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow: '<span class="left"><i class="fa-solid fa-chevron-left"></i></span>',
+    nextArrow:'<span class="right"><i class="fa-solid fa-chevron-right"></i></span>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+    
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+   // shop details slick ends
+   var mixer = mixitup('.mix', {
+    load: {
+      filter: '.category-a'
+  }  
+});
+
+
+})
+
+
+
+// shop insta
+$(function(){
+  $('.parent_expert').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,  
+    autoplaySpeed: 2000,
+    arrows:true,
+    prevArrow: '<span class="left"><i class="fa-solid fa-chevron-left"></i></span>',
+    nextArrow:'<span class="right"><i class="fa-solid fa-chevron-right"></i></span>',
+    dots:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
+
+  $('.parent_insta').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,  
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
+})
+
+  
+
+$(function(){
+
+  let crossBtn = document.querySelector('.cross-btn');
+let searchBtn = document.querySelector('.search_btn');
+let searchBox = document.querySelector('.search-box');
+let inputTest = document.querySelector('.input-test');
+
+
+searchBox.addEventListener('click',function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('active_search')){
+        searchBox.classList.remove('active_search');
+    }
+});
+
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    searchBox.classList.add('active_search');
+    if(searchBox.classList.contains('active_search')){
+        inputTest.classList.add('active_input');
+        }else{
+        inputTest.classList.remove('active_input');
+        }
+        });
+        
+ crossBtn.addEventListener('click', function(e){
+            e.preventDefault();
+            searchBox.classList.remove('active_search');
+            inputTest.classList.remove('active_input');
+    
+});
+
+    $('.main_slider').slick({
+        arrows:false,
+        dots:true,
+        dotsClass:'bannerDots container',
+        autoplay: true,
+        autoplaySpeed: 1800,
+    });
+
+    //* ARRIVALS JS 
+    $('.parent_arrivals').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        prevArrow: '<span class="left"><i class="fa-solid fa-chevron-left"></i></span>',
+        nextArrow:'<span class="right"><i class="fa-solid fa-chevron-right"></i></span>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+        
+          {
+            breakpoint: 590,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+
+        ]
+      });
+    //* ARRIVALS JS END
+
+    //tooltip 
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    //tooltip ends
+
+   
+      
+      // latest
+    $('.latest').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,  
+      autoplaySpeed: 2000,
+      arrows:false,
+      dots:true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+       
+      ]
+    });
+    // latest ends
+   
+
+    // DEAL
+    $('.counter_parent').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      arrows:false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    // DEAL ENDS
+    // expert
+
+   
+
+// expert ends
+})
+
+
+// today venobox
+
+new VenoBox({
+  selector: '.explore',
+});
+
+// today venobox
+var mixitup = require('mixitup');
+
+
+
